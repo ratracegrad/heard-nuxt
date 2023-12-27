2858718e05ee9a8d99de50003e7605a3193e0f58
Jennifer Bland <ratracegrad@gmail.com>
2023-12-27 18:27:26 -0500

    Renamed functions to be consistent with CRUD

--- pages/index.vue
+++ pages/index.vue
@@ -11,12 +11,16 @@ interface Transaction {
   toAccount: string
   transactionDate: string
 }
+/* --- End of TypeScript Types --- */
+
 
 /* --- Variables --- */
-const loading = ref(true)
 const transactions = ref<Transaction[]>([])
+/* --- End of Variables --- */
 
-async function getTransactions() {
+
+/* --- Functions --- */
+async function readTransactions() {
   const { data } = await supabase
     .from('transactions')
     .select(`id, title, description, amount, fromAccount, toAccount, transactionDate`)
@@ -26,21 +30,25 @@ async function getTransactions() {
     transactions.value = data
 }
 
-function addTransaction() {
+function createTransaction() {
   console.log('button clicked')
 }
 
-function editTransaction(id: number) {
+function updateTransaction(id: number) {
   console.log('button clicked', id)
 }
 
 function deleteTransaction(id: number) {
   console.log('button clicked', id)
 }
+/* --- End of Functions --- */
 
+
+/* --- Lifecycle Hooks --- */
 onMounted(() => {
-  getTransactions()
+  readTransactions()
 })
+/* --- End of Lifecycle Hooks --- */
 </script>
 
 <template>
@@ -49,7 +57,7 @@ onMounted(() => {
       Transactions List
     </h2>
     <div class="flex justify-end">
-      <button class="mr-2 rounded bg-gray-300 px-4 py-2 text-black font-bold hover:bg-gray-500" @click="addTransaction()">
+      <button class="mr-2 rounded bg-gray-300 px-4 py-2 text-black font-bold hover:bg-gray-500" @click="createTransaction()">
         + Add
       </button>
     </div>
@@ -107,7 +115,7 @@ onMounted(() => {
               {{ transaction.transactionDate }}
             </td>
             <td class="border border-gray-400 px-4 py-2">
-              <button class="mr-2 rounded bg-blue-500 px-4 py-2 text-white font-bold hover:bg-blue-700" @click="editTransaction(transaction.id)">
+              <button class="mr-2 rounded bg-blue-500 px-4 py-2 text-white font-bold hover:bg-blue-700" @click="updateTransaction(transaction.id)">
                 Edit
               </button>
               <button class="rounded bg-red-500 px-4 py-2 text-white font-bold hover:bg-red-700" @click="deleteTransaction(transaction.id)">
