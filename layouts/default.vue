58c95233e4bcb5023156552da5b81138508d714a
Jennifer Bland <ratracegrad@gmail.com>
2023-12-27 18:04:06 -0500

    Removed all components that came with default template since they will not be used. Removed secondary layouts. On home page tested that could get data from Supabase via console log

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <main class="px-10 py-20 text-center">
+  <main class="mx-auto max-w-7xl flex flex-col">
     <slot />
   </main>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,22 @@
 <script setup lang="ts">
-const online = useOnline()
+const supabase = useSupabaseClient()
+
+async function getTransactions() {
+  const { data } = await supabase
+    .from('transactions')
+    .select(`id, title, description, amount, fromAccount, toAccount, transactionDate`)
+    .order('id', { ascending: true })
+
+  if (data) 
+    console.log('data', data)
+  
+}
+
+onMounted(() => {
+  getTransactions()
+})
+
+
 </script>
 
 <template>
